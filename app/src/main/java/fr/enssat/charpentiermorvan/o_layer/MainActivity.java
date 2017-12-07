e0a96b51fc155db1a1c163e93d63e9bbc13e0c95
benjent <Benjent@hotmail.fr>
2017-12-07 09:02:21 +0100

    First commit

---
+++ app/src/main/java/fr/enssat/charpentiermorvan/o_layer/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.enssat.charpentiermorvan.o_layer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
