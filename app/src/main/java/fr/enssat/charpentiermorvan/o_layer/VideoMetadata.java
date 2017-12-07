f6db8743bc716df48db8d67bdfe1b4701dc40e3c
benjent <Benjent@hotmail.fr>
2017-12-07 09:46:53 +0100

    Create class, attributes and getters-setters

---
+++ app/src/main/java/fr/enssat/charpentiermorvan/o_layer/VideoMetadata.java
@@ -0,0 +1,44 @@
+package fr.enssat.charpentiermorvan.o_layer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by benjent on 07/12/17.
+ */
+
+public class VideoMetadata {
+
+    int id;
+    String name;
+    ArrayList tags;
+
+    public VideoMetadata(Object object) {
+
+        this.id = object.getId();
+        this.name = object.getName();
+        this.tags = object.getTags();
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList getTags() {
+        return tags;
+    }
+
+    public void addTag(String tag) {
+        if(tag != null) {
+            this.tags.add(tag);
+        }
+    }
+}
